df08a166b50781ae5a8ee67ffe459266768571ec
Min-ky <titma326@daum.net>
2022-07-13 21:14:24 +0900

    관리자 회원 목록 조회 추가!

--- src/main/java/com/kh/icodi/member/model/service/MemberService.java
+++ src/main/java/com/kh/icodi/member/model/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.kh.icodi.member.model.service;
 
 import static com.kh.icodi.common.JdbcTemplate.*;
+
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
 import com.kh.icodi.member.model.dao.MemberDao;
 import com.kh.icodi.member.model.dto.Member;
@@ -21,6 +24,51 @@ public class MemberService {
 		int result = 0;
 		try {
 			result = memberDao.insertMember(conn, member);
+			commit(conn);
+		}
+		catch(Exception e) {
+			rollback(conn);
+			throw e;
+		}
+		finally {
+			close(conn);
+		}
+		return result;
+	}
+
+	public List<Member> findMemberLike(Map<String, Object> param) {
+		Connection conn = getConnection();
+		List<Member> list = memberDao.findMemberLike(conn, param);
+		close(conn);
+		return list;
+	}
+
+	public int getTotalContentLike(Map<String, Object> param) {
+		Connection conn = getConnection();
+		int totalContent = memberDao.getTotalContentLike(conn, param);
+		close(conn);
+		return totalContent;
+	}
+
+	public List<Member> findAll(Map<String, Object> param) {
+		Connection conn = getConnection();
+		List<Member> list = memberDao.findAll(conn, param);
+		close(conn);
+		return list;
+	}
+
+	public int getTotalContent() {
+		Connection conn = getConnection();
+		int totalContent = memberDao.getTotalContent(conn);
+		close(conn);
+		return totalContent;
+	}
+
+	public int updateMemberRole(Member member) {
+		Connection conn = getConnection();
+		int result = 0;
+		try {
+			result = memberDao.updateMemberRole(conn, member);
 			commit(conn);
 		}
 		catch(Exception e) {
