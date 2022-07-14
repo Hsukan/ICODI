cba0bc7649e3cd95d473d174f8753c7f93984db6
chaenyy <chaeeun0119@naver.com>
2022-07-14 17:29:33 +0900

    codiBook 추후 css 작업예정

---
+++ src/main/java/com/kh/icodi/codiBook/model/service/CodiBookService.java
@@ -0,0 +1,28 @@
+package com.kh.icodi.codiBook.model.service;
+
+import static com.kh.icodi.common.JdbcTemplate.close;
+import static com.kh.icodi.common.JdbcTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.kh.icodi.admin.model.dto.ProductAttachment;
+import com.kh.icodi.admin.model.dto.ProductExt;
+import com.kh.icodi.codiBook.model.dao.CodiBookDao;
+
+public class CodiBookService {
+	private CodiBookDao codiBookDao = new CodiBookDao();
+
+	public List<ProductExt> findAllByCategoryNo(int categoryNo) {
+		Connection conn = getConnection();
+		List<ProductExt> products = codiBookDao.findAllByCategoryNo(conn, categoryNo);
+		if(products != null && !products.isEmpty()) {
+			for(ProductExt product : products) {
+				List<ProductAttachment> attachments = codiBookDao.findAttachmentByProductCode(conn, product.getProductCode());
+				product.setAttachmentList(attachments);
+			}
+		}
+		close(conn);
+		return products;
+	}
+}
