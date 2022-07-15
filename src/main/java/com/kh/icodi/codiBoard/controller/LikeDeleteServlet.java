da85a84ce55e7556e202ff9318c6f33c649c1f26
chaenyy <chaeeun0119@naver.com>
2022-07-15 21:49:25 +0900

    Merge branch 'master' of https://github.com/Hsukan/icodi.git

--- src/main/java/com/kh/icodi/codiBoard/controller/LikeDeleteServlet.java
+++ src/main/java/com/kh/icodi/codiBoard/controller/LikeDeleteServlet.java
@@ -1,11 +1,15 @@
 package com.kh.icodi.codiBoard.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.icodi.codiBoard.model.dto.LikeThat;
+import com.kh.icodi.codiBoard.model.service.CodiBoardService;
 
 /**
  * Servlet implementation class LikeDeleteServlet
@@ -13,12 +17,22 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/codiBoard/likeDelete")
 public class LikeDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private CodiBoardService codiBoardService = new CodiBoardService();
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+			int codiBoardNo = Integer.parseInt(request.getParameter("codiBoardNo"));
+			String memberId = request.getParameter("memberId");
+			LikeThat delLike = new LikeThat(0, memberId, codiBoardNo);
+			
+			int result = codiBoardService.deleteLike(delLike);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
 	}
-
 }
