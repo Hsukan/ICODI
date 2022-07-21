34fb1bbd96e2dcae41c24b2723c473b0a94e59b5
Hsukan <kei01105@naver.com>
2022-07-21 14:17:48 +0900

    코디상품 보기 + 인기코디 추가

--- src/main/java/com/kh/icodi/codiBoard/model/dao/CodiBoardDao.java
+++ src/main/java/com/kh/icodi/codiBoard/model/dao/CodiBoardDao.java
@@ -79,6 +79,31 @@ public class CodiBoardDao {
 		}
 		return codiBoardList;
 	}
+	public List<CodiBoardExt> findHotCodiBoard(Connection conn, Map<String, Object> param) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		List<CodiBoardExt> codiBoardList = new ArrayList<>();
+		String sql = prop.getProperty("findHotCodiBoard");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, (String)param.get("memberId"));
+			pstmt.setInt(2, (int)param.get("start"));
+			pstmt.setInt(3, (int)param.get("end"));
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				CodiBoardExt codiBoardExt = handleCodiBoardResultSet(rset);
+				codiBoardExt.setLikedMember(rset.getString("liked_member"));
+				codiBoardList.add(codiBoardExt);
+			}
+		} catch (SQLException e) {
+			throw new CodiBoardException("로그인 회원 최신 코디게시판 조회 오류!", e);
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		return codiBoardList;
+	}
 	
 	// 좋아요 유무 조회
 	// checkLiked = select * from like_that where member_id = ? and codi_board_no = ?
